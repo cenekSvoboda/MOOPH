5e433dc3688d34a41fb1be7b88e6b49300fc568a
cenekSvoboda <svobo.c@gmail.com>
2013-09-05 12:57:33 +0200

    Created
    ...

---
+++ moophDevelopment.js
@@ -0,0 +1,87 @@
+/*
+ * MOOPH = Module for Object Oriented Pseudomultithreaded Hypertext
+ * Copyright 2013 Cenek Svoboda, svobo.c@gmail.com
+ */
+var MOOPH=(function MOOPH()
+{
+	var _falseThreads=[];
+	return {
+		createFalseThread:function createFalseThread()
+		{
+			var _ftid=_falseThreads.length;
+			_falseThreads[_ftid]={};
+			_falseThreads[_ftid].pause=true;
+			_falseThreads[_ftid].isMeasuredFromLastTime=false;
+			_falseThreads[_ftid].isMeasuredBeforeCall=true;
+			_falseThreads[_ftid].timeoutDelay=1;
+			_falseThreads[_ftid].measuredDelay=0;
+			_falseThreads[_ftid].lastTime=0;
+			_falseThreads[_ftid].falseThread=function falseThread(ftid)
+			{
+				if(_falseThreads[ftid].pause===true){
+					return;
+				}
+				var now=new Date().getTime();
+				if(now>_falseThreads[ftid].lastTime+_falseThreads[ftid].measuredDelay){
+					var imbc=_falseThreads[ftid].isMeasuredBeforeCall;
+					if(!imbc){
+						_falseThreads[ftid].funct(_falseThreads[ftid].parameters);
+					}
+					if(_falseThreads[ftid].isMeasuredFromLastTime){
+						_falseThreads[ftid].lastTime+=_falseThreads[ftid].measuredDelay;
+					}else{
+						if(!imbc){
+							_falseThreads[ftid].lastTime=new Date().getTime();
+						}else{
+							_falseThreads[ftid].lastTime=now;
+						}
+					}
+					if(imbc){
+						_falseThreads[ftid].funct(_falseThreads[ftid].parameters);
+					}
+				}
+				var fun=function fun()
+				{
+					_falseThreads[ftid].falseThread(ftid);
+				};
+				setTimeout(fun,_falseThreads[ftid].timeoutDelay);
+			};
+			return {
+				setFunction:function setFunction(funct)
+				{
+					_falseThreads[_ftid].funct=funct;
+				},
+				setParameters:function setParameters(parameters)
+				{
+					_falseThreads[_ftid].parameters=parameters;
+				},
+				setTimeoutDelay:function setTimeoutDelay(timeoutDelay)
+				{
+					_falseThreads[_ftid].timeoutDelay=timeoutDelay;
+				},
+				setMeasuredDelay:function setMeasuredDelay(measuredDelay)
+				{
+					_falseThreads[_ftid].measuredDelay=measuredDelay;
+				},
+				setMeasuredFromLastTime:function setMeasuredFromLastTime(isMeasuredFromLastTime)
+				{
+					_falseThreads[_ftid].isMeasuredFromLastTime=isMeasuredFromLastTime;
+				},
+				setMeasuredBeforeCall:function setMeasuredBeforeCall(isMeasuredBeforeCall)
+				{
+					_falseThreads[_ftid].isMeasuredBeforeCall=isMeasuredBeforeCall;
+				},
+				start:function start()
+				{
+					_falseThreads[_ftid].pause=false;
+					_falseThreads[_ftid].lastTime=new Date().getTime();
+					_falseThreads[_ftid].falseThread(_ftid);
+				},
+				pause:function pause()
+				{
+					_falseThreads[_ftid].pause=true;
+				}
+			};
+		}
+	};
+}());
